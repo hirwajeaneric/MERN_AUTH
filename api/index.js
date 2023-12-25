130a15ee7fdcb09679102df46fae417a99ad4f72
hirwajeaneric <hirwajeaneric25@gmail.com>
2023-12-25 10:32:16 +0200

    Connect to DB

--- api/index.js
+++ api/index.js
@@ -1,7 +1,15 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import colors from 'colors';
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB_URI)
+.then((res)=>console.log('> Database Connected...'.bgCyan))
+.catch(err=>console.log(`> Error while connecting to mongoDB : ${err.message}`.underline.red ))
 
 const app = express();
 
 app.listen(3000, () => {
-    console.log('Server listening on port 3000');
+    console.log('> Server listening on port 3000...'.bgMagenta);
 });
