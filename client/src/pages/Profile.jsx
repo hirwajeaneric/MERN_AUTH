dcdfef763c50f3e1c16cc6bf6c4e32db290a955e
hirwajeaneric <hirwajeaneric25@gmail.com>
2024-01-01 13:35:32 +0200

    Add signout functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -2,7 +2,15 @@ import { useSelector, useDispatch } from 'react-redux';
 import { useEffect, useRef, useState } from 'react';
 import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage';
 import { app } from '../firebase';
-import { updateUserStart, updateUserSuccess, updateUserFailure, deleteUserFailure, deleteUserStart, deleteUserSuccess } from '../redux/user/userSlice';
+import { 
+  updateUserStart, 
+  updateUserSuccess, 
+  updateUserFailure, 
+  deleteUserFailure, 
+  deleteUserStart, 
+  deleteUserSuccess, 
+  signOut
+} from '../redux/user/userSlice';
 
 const Profile = () => {
   
@@ -86,6 +94,8 @@ const Profile = () => {
   // DELETE ----------------------------------------------------------------
   const handleDeleteAccount = async () => {
     try {
+      dispatch(deleteUserStart());
+
       const res = await fetch(`/api/user/delete/${currentUser._id}`, {
         method: 'DELETE',
       });
@@ -101,7 +111,17 @@ const Profile = () => {
   }
 
 
-  
+  // SIGNOUT ----------------------------------------------------------------
+  const handleSignout = async () => {
+    try {
+      await fetch(`/api/auth/signout`);
+      dispatch(signOut());
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -171,7 +191,7 @@ const Profile = () => {
       </form>
       <div className='flex justify-between mt-5'>
         <span onClick={handleDeleteAccount} className='text-red-700 cursor-pointer'>Delete Account</span>
-        <span className='text-red-700 cursor-pointer'>Sign out</span>
+        <span onClick={handleSignout} className='text-red-700 cursor-pointer'>Sign out</span>
       </div>
       <p className='text-red-700 mt-5'>{error && 'Something went wrong!'}</p>
       <p className='text-green-700 mt-5'>{updateSuccess && 'User is updated successfully!'}</p>
